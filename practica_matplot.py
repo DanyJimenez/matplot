0ebf883d872321f04cb87b59a6120ad3fa49c595
Deivis Dany Jimenez Hernandez <DeivisJimenez@comfama.com.co>
2023-02-20 22:40:36 -0500

    se corrigen errores

--- practica_matplot.py
+++ practica_matplot.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-'''
+
 country = ['Colombia', 'Ecuador', 'México']
 population = [50, 12, 129]
 colors = ['yellow', 'red', 'green']
@@ -12,8 +12,8 @@ plt.xlabel('Country')
 plt.ylabel('Population')
 plt.show()
 
-'''
-'''
+
+
 mes = ['ene', 'feb', 'mar', 'abr', 'may', 'jun']
 promedio_temperatura = [29, 32, 31, 28, 27, 29 ]
 
@@ -28,9 +28,7 @@ plt.ylabel('Promedio de temperatura (°c)')
 plt.legend()
 plt.show()
 
-'''
 
-'''
 
 
 gastos = ['Servicios públicos', 'Alimentación', 'Transporte', 'Arriendo', 'Recreación', 'Salud', 'Ahorro']
@@ -45,7 +43,10 @@ plt.title('Porcentaje de gastos mensuales')
 #plt.legend(loc='upper left')
 plt.show()
 
-'''
+
+
+
+
 tuits = ['Experiencias\n personales', 'Política', 'Religión', 'Deporte', 'Tecnología']
 num_tuits = [314000, 1346800, 400000, 1800000, 950000]
 total_tuits = sum(num_tuits)
@@ -55,3 +56,5 @@ plt.pie(num_tuits, labels= tuits, autopct='%1.1f%%')
 plt.title('Temáticas que más se postean en Twitter Colombia')
 plt.text(-1.5,-1.5,f'Muestra: {total_tuits} tuits', fontsize=12)
 plt.show()
+
+
